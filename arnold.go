f988856fbd16e5f5fb1559d01848288ebde98f7f
abmutungi <abmutungi@gmail.com>
2021-09-18 17:02:14 +0100

    create function to print grid

--- arnold.go
+++ arnold.go
@@ -1,4 +1,25 @@
 package raid2
 
-func DrawGrid([][]int) {
+import "fmt"
+
+func DrawGrid(grid [][]int) {
+	/* grid := [9][9]int{ // a slice of a slice with 9 rows and 9 columns
+		{1, 2, 3, 4, 5, 6, 7, 8, 9},
+		{1, 2, 3, 4, 5, 6, 7, 8, 9},
+		{1, 2, 3, 4, 5, 6, 7, 8, 9},
+		{1, 2, 3, 4, 5, 6, 7, 8, 9},
+		{1, 2, 3, 4, 5, 6, 7, 8, 9},
+		{1, 2, 3, 4, 5, 6, 7, 8, 9},
+		{1, 2, 3, 4, 5, 6, 7, 8, 9},
+		{1, 2, 3, 4, 5, 6, 7, 8, 9},
+		{1, 2, 3, 4, 5, 6, 7, 8, 9},
+	}
+	*/
+	for i := 0; i < 9; i++ { // declare i and j as integers
+		for j := 0; j < 9; j++ {
+			fmt.Print(grid[i][j])
+			fmt.Print(" ")
+		}
+		fmt.Println()
+	}
 }
