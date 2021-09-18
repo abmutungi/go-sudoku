bb474f0d1058f28adf7322f7cefdff3d61eeced1
Nik <nishak.don@gmail.com>
2021-09-18 19:12:04 +0100

    Fix variable input

--- arnold.go
+++ arnold.go
@@ -2,7 +2,7 @@ package raid2
 
 import "fmt"
 
-func DrawGrid(grid [][]int) {
+func DrawGrid(grid [9][9]int) {
 	/* grid := [9][9]int{ // a slice of a slice with 9 rows and 9 columns
 		{1, 2, 3, 4, 5, 6, 7, 8, 9},
 		{1, 2, 3, 4, 5, 6, 7, 8, 9},
