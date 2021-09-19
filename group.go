3f6ee651c1cfa555ea28d6a2540af3f82633cc09
Nik <nishak.don@gmail.com>
2021-09-19 20:27:53 +0100

    add the sudoku solver function

---
+++ group.go
@@ -0,0 +1,20 @@
+package raid2
+
+func SolveSudoku(grid *[9][9]int) {
+	for y := 0; y < 9; y++ {
+		for x := 0; x < 9; x++ {
+			if grid[y][x] == 0 {
+				for value := 1; value <= 9; value++ {
+					if CheckPossible(grid, value, y, x) {
+						grid[y][x] = value
+						SolveSudoku(grid)
+						grid[y][x] = 0
+					}
+				}
+				return
+			}
+		}
+	}
+	DrawGrid(grid)
+	return
+}
