921cf0771f39889b341a0bccc766871b0fa78aa3
Nik <nishak.don@gmail.com>
2021-09-19 22:17:51 +0100

    Fix search for 1 solution

--- group.go
+++ group.go
@@ -1,13 +1,13 @@
 package raid2
 
-func SolveSudoku(grid *[9][9]int) {
+func SolveSudoku(grid *[9][9]int, solutions *int) {
 	for y := 0; y < 9; y++ {
 		for x := 0; x < 9; x++ {
 			if grid[y][x] == 0 {
 				for value := 1; value <= 9; value++ {
 					if CheckPossible(grid, value, y, x) {
 						grid[y][x] = value
-						SolveSudoku(grid)
+						SolveSudoku(grid,solutions)
 						grid[y][x] = 0
 					}
 				}
@@ -15,6 +15,9 @@ func SolveSudoku(grid *[9][9]int) {
 			}
 		}
 	}
-	DrawGrid(grid)
+	if *solutions<2{
+		DrawGrid(grid)
+	}
+	*solutions ++
 	return
 }
